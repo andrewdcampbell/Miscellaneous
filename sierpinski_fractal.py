904f49040d60a4e32eb8c70f36e633d734a7a6b1
Andy <andrewdcampbell@berkeley.edu>
2017-01-24 14:41:34 -0800

    Deleted old screenshots

--- sierpinski_fractal.py
+++ sierpinski_fractal.py
@@ -43,7 +43,7 @@ class SierpinskiTriangle(object):
 		return random.choice(self.vertices)
 
 	def _getCoord(self, x, y):
-		return x - self.midX, y - self.midY - 10
+		return x - self.midX, y - self.midY - 50
 
 	def getMidpoint(self, p1, p2):
 		x1, y1 = p1
@@ -51,7 +51,7 @@ class SierpinskiTriangle(object):
 		return ((x1 + x2) / 2), ((y1 + y2) / 2)
 
 	def getColor(self):
-		colors = ["blue", "orange"]
+		colors = ["white", "green"]
 		return random.choice(colors)
 		
 	def draw(self, iterations):
@@ -66,7 +66,7 @@ class SierpinskiTriangle(object):
 			x, y = mp
 			prevPoint = mp
 			self.t.setpos(x, y)
-			self.t.dot(0, self.getColor())
+			self.t.dot(1, self.getColor())
 			count += 1
 			updateCount += 1
 			if updateCount == 200:
@@ -79,22 +79,60 @@ class SierpinskiTriangle(object):
 		self.wn.exitonclick()
 
 class SierpinskiSquare(SierpinskiTriangle):
-	"""docstring for SierpinskiSquare"""
+	"""A point inside a square repeatedly jumps half of the distance towards 
+	   a randomly chosen vertex, but the currently chosen vertex cannot be the 
+	   same as the previously chosen vertex.
+	"""
 	def __init__(self):
 		super(SierpinskiSquare, self).__init__()
 		ll = (10, self._getY(10, True))
 		ul = (10, self._getY(self.midX, True))
 		lr = (self.w - 10, self._getY(10, True))
 		ur = (self.w - 10, self._getY(self.midX, True))
-		self.vertices = [self._getCoord(p[0], p[1]) for p in [ll, ul, lr, ur]]
+		self.vertices = [self._getCoord(p[0], p[1]) for p in [ll, ul, ur, lr]]
 
 	def getRandomVertex(self, lastVertex):
-		v = random.choice(self.vertices)
-		while v == lastVertex:
-			v = random.choice(self.vertices)
-		return v
+		index = random.randrange(0,len(self.vertices))
+		lastIndex = -1
+		if lastVertex:
+			lastIndex = self.vertices.index(lastVertex)
+		while index == lastIndex:
+			index = random.randrange(0,len(self.vertices))
+		return self.vertices[index]
+
+class SierpinskiSquare2(SierpinskiSquare):
+	"""A point inside a square repeatedly jumps half of the distance 
+	   towards a randomly chosen vertex, but the currently chosen 
+	   vertex cannot be 1 place away from the previously chosen vertex.
+	"""
+
+	def getRandomVertex(self, lastVertex):
+		index = random.randrange(0,len(self.vertices))
+		lastIndex = -1
+		if lastVertex:
+			lastIndex = self.vertices.index(lastVertex)
+		while (index + 1) % 4 == lastIndex:
+			index = random.randrange(0,len(self.vertices))
+		return self.vertices[index]
+
+class SierpinskiSquare3(SierpinskiSquare):
+	"""A point inside a square repeatedly jumps half of the distance 
+	   towards a randomly chosen vertex, but the currently chosen 
+	   vertex cannot be 1 place away from the previously chosen vertex.
+	"""
+	def getRandomVertex(self, lastVertex):
+		index = random.randrange(0,len(self.vertices))
+		lastIndex = -1
+		if lastVertex:
+			lastIndex = self.vertices.index(lastVertex)
+		while (index + 2) % 4 == lastIndex:
+			index = random.randrange(0,len(self.vertices))
+		return self.vertices[index]
 
 
-# s = SierpinskiTriangle()
-s = SierpinskiSquare()
-s.main(1000000)
+# Uncomment one of the following instantiations! Also try changing the color list!
+s = SierpinskiTriangle()
+# s = SierpinskiSquare()
+# s = SierpinskiSquare2()
+# s = SierpinskiSquare3()
+s.main(20000)
